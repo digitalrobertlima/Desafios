10410e57065b171ed789a7f118b6e42b9cbe5293
Robert Lima <ge.crio123@gmail.com>
2020-08-31 12:47:10 -0300

    Script de leitura de dados
    Criando um script Python, que leia o nome de uma pessoa e mostre uma mensagem de Boas-Vindas de acordo com o valor digitado.

---
+++ tests.py
@@ -0,0 +1,3 @@
+confirm = input('Welcome! Tás a ver que estas confirmações ocorrerão apenas uma única vez em toda essa Blockchain? Atenção ao que vai escrever! (S/n)')
+nome = input('Como você se identifica para a Rede?')
+idade = input('Em qual era da humanidade tu estás a ver este início?')
